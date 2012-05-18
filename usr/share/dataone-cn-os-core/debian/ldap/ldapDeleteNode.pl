892e3a12b22014a6a4ebc2bad22507f7c7cb0d1b
Robert Waltz <rwaltz@utk.edu>
2012-05-18 19:05:10 +0000

    would have been helpful to have committed this file before...

--- usr/share/dataone-cn-os-core/debian/ldap/ldapDeleteNode.pl
+++ usr/share/dataone-cn-os-core/debian/ldap/ldapDeleteNode.pl
@@ -17,24 +17,27 @@ sub usage {
 
    print STDERR (
       $message,
-      "usage: $command [--help] --id nodeId\n" 
+      "usage: $command [--help] --password password [--id nodeId] | [--all]\n" 
    );
 
    die("\n")
 }
 
-
+my $password;
 my $nodeid;
 my $all;
 my $help;
 
 Getopt::Long::GetOptions(
    'id=s' => \$nodeid,
+   'password=s' => \$password,
    'all' => \$all,
    'help!' => \$help
 ) or usage("Invalid commmand line options.");
  
 usage() if (defined $help);
+
+usage() if (!defined $password);
 
 usage("The identifier of the node must be specified.")
    unless ((defined $nodeid) || (defined $all));
@@ -42,7 +45,30 @@ usage("The identifier of the node must be specified.")
 
 my $ldap = Net::LDAP->new("localhost");
 
-my $mesg = $ldap->bind("cn=admin,dc=dataone,dc=org", password=>"password");
+my $mesg = $ldap->start_tls(verify => 'none',
+    clientcert => 'REPLICATION_CERTIFICATE',
+    clientkey => 'REPLICATION_CERTIFICATE_KEY'
+);
+
+my $mesg = $ldap->bind("cn=admin,dc=dataone,dc=org", password=>${password});
+
+
+if (defined $all) 
+	{
+	$mesg = $ldap->search(  filter=>"(objectClass=d1ServiceMethodRestriction)", 
+                      base=>"dc=dataone,dc=org");
+    } else {
+    $mesg = $ldap->search(  filter=>"(&(objectClass=d1ServiceMethodRestriction) (d1NodeId=$nodeid))", 
+    	base=>"dc=dataone,dc=org");
+    }
+my @entries = $mesg->entries;
+foreach my $entry (@entries) 
+	{
+	my $dn = $entry->dn();
+	print "DN = $dn\n";
+	$ldap->delete($dn);
+	}
+
 
 if (defined $all) 
 	{
